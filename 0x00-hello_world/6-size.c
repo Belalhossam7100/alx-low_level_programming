ea10288e546c180d9060e390bed18fb57a63b7aa
Belalhossam7100 <redhood7100@gmail.com>
2023-08-12 16:20:37 -0700

    dd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+	float f:
+	long int lf;
+	long long int llf;
+	printf("Size of an int : %lu.\n", (unsigned long) sizeof(i));
+	printf("Size of float: %lu.\n", (unsigned long) sizeof(f));
+	printf("Size of a char: %lu.\n", (unsigned long) sizeof(c));
+	printf("Size of a long int; %lu.\n",(unsigned long) sizeof(lf);
+	printf("Size of a long long int:; %ln.\n", (unsigned long) sizeof(llf);
+	return (0);
+}
