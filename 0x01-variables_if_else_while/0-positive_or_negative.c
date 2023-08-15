d026f07e740989ad0545a5cfaa0425007224119a
Belalhossam7100 <redhood7100@gmail.com>
2023-08-14 17:15:27 -0700

    fff

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero \n');
+	printf("%d is zero \n");
 	}
 	else if (n < 0)
-	{
-	printf(" %d is negative \n");
-	}
+        {
+        printf("%d is zero \n");
+        }
 	return (0);
 }
