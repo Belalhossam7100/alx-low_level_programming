7ee09e0f96845b8cfd6d0d0a6bb590843ae991bd
Belalhossam7100 <redhood7100@gmail.com>
2023-08-16 14:53:00 -0700

    fdvd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,23 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * maiin - check the code
  *
  * Return: Always none
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-        int i;
-	int j;
-                for (i = 97; i <= 122; i++)
-        {
-		for (j = 0; i <= 5; j++)
-		{	
-                _putchar(i);
-		}
-        _putchar('\n');
-	}
+int i;
+int j;
+
+for (i = 97; i <= 122; i++)
+{
+for (j = 0; i <= 5; j++)
+{	
+_putchar(i);
+}
+_putchar('\n');
+}
+return;
 }
