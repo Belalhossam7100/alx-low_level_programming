76b99359a4031d78971bd73ee7f14d22cef9d95f
Belalhossam7100 <redhood7100@gmail.com>
2023-08-16 15:29:13 -0700

    kg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,16 @@ void print_alphabet_x10(void)
 {
 int i;
 int j;
+j=0;
 
+for (j = 0; i <= 10; j++)
+{
 for (i = 97; i <= 122; i++)
-{       
-for (j = 0; j <= 10; j++) 
 {
 _putchar(i);
+{
 _putchar('\n');
+}
 }
 }
 }
